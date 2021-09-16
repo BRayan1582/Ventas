8546c8144ae6b03713123eb36c0e0a7d1db4c6f7
BRayan1582 <brayan1uf@gmail.com>
2021-09-16 11:28:20 -0600

    Creando Hola Mundo

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola mundo");
+            MessageBox.Show("El único modo de hacer un gran trabajo es amar lo que haces");
         }
     }
 }
